df6bf87d7b2dc5de9164380ccfc2516362eb9f21
Lingxiao Li <lingxiaoli98@gmail.com>
2022-01-31 06:21:14 +0800

    Delete alibi_detect directory

--- alibi_detect/cd/__init__.py
+++
@@ -1,9 +0,0 @@
-from .chisquare import ChiSquareDrift
-from .ks import KSDrift
-from .model_uncertainty import ClassifierUncertaintyDrift
-
-__all__ = [
-    "ChiSquareDrift",
-    "KSDrift",
-    "ClassifierUncertaintyDrift"
-]
